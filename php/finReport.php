18f7f4c69417ed5b69cd60041aecbe49cf372b46
songa1 <songachillethe1her@gmail.com>
2023-01-10 18:16:00 +0200

    I have added some roles check

--- php/finReport.php
+++ php/finReport.php
@@ -1,4 +1,23 @@
 <?php
 
+$results = $conn->query($sql);
+
+if($results->num_rows > 0){
+    while($row = $results->fetch_assoc()) {
+        ?>
+        <tbody>
+            <tr>
+                <td><?php echo $row['role_id'] ?></td>
+                <td><?php echo $row['role_name'] ?></td>
+                <td><?php echo $row['role_desc'] ?></td>
+                <td>
+                    <button class="btn success">Edit</button>
+                    <a href="../functions/deleteroles.php?id=<?php echo $row['role_id']; ?>" class="btn danger" >Delete</a>
+                </td>
+            </tr>                       
+        </tbody>
+        <?php
+    }
+}
 
 ?>
